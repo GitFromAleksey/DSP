813448f52e498b5c4603baebbacd0b0160252dd6
Aleksey <a_h.79@mail.ru>
2020-06-28 00:36:52 +0700

    Исправлена ошибка генерации синусов. По оси Х должно быть время.

--- 1.1/1.1.py
+++ 1.1/1.1.py
@@ -7,24 +7,38 @@ import SignalGenerate as sg
 
 print('Глава 1.1')
 
+##2^(1/12) = 1.059
+##2^(2/12) = 1.122
+##2^(3/12) = 1.189
+##2^(4/12) = 1.260
+##2^(5/12) = 1.335
+##2^(6/12) = 1.414
+##2^(7/12) = 1.498
+##2^(8/12) = 1.587
+##2^(9/12) = 1.682
+##2^(10/12) = 1.782
+##2^(11/12) = 1.888
+##2^(12/12) = 2
 
-
-f0 = 2 # частота сигнала, Гц
+f0 = 1 # частота сигнала, Гц
 A = 1 # амплитуда сигнала в о.е.
-n = 15 # количество отсчётов на период
-n_T = 1 # кол-во периодов
+ts = 0.001 # время дискретизации, сек
+time = 10 # время генерации, сек
 ShiftZero = 0 # сдвиг относительно оси "х" о.е. амплитуды
-ShiftPhaseDegree = 180 # сдвиг фазы в градусах
+ShiftPhaseDegree = 0 # сдвиг фазы в градусах
 
-_x, _y = sg.getSinus(f0, A, n, n_T, ShiftZero, ShiftPhaseDegree)
+_t, _y = sg.getSinus(f0, A, ts, time, ShiftZero, ShiftPhaseDegree)
 
-_x1, _y1 = sg.getSinus(f0*2, A, n, n_T, ShiftZero, ShiftPhaseDegree)
+_t1, _y1 = sg.getSinus(f0*(2**(3/12)), A, ts, time, ShiftZero, ShiftPhaseDegree)
+_ys = _y + _y1
 
 plt.title('Title')
-plt.xlabel('n_array')
-plt.ylabel('_y(n_array)')
+plt.xlabel('t, s')
+plt.ylabel('y(t)')
 plt.grid(True)
-plt.plot(_x, _y)
-plt.plot(_x1, _y1)
-plt.scatter(_x, _y)
+plt.plot(_t, _ys)
+plt.plot(_t, _y)
+plt.plot(_t1, _y1)
+##plt.scatter(_t, _y)
+##plt.scatter(_t1, _y1)
 plt.show()
--- 1.1/SignalGenerate.py
+++ 1.1/SignalGenerate.py
@@ -7,18 +7,19 @@ SelfFileName = os.path.basename(__file__)
 
 # f0 - частота сигнала, Гц
 # A - амплитуда сигнала в о.е.
-# n - количество отсчётов на период
-# n_T - кол-во периодов
+# ts - время дискретизации, сек
+# time - время генерации сигнала
 # ShiftZero - сдвиг относительно оси "х" о.е. амплитуды
 # ShiftPhaseDegree - сдвиг фазы в градусах
-def getSinus(f0, A, n, n_T, ShiftZero, ShiftPhaseDegree):
+def getSinus(f0, A, ts, time, ShiftZero, ShiftPhaseDegree):
     print()
     print(SelfFileName,'-> Sinus generate begin:')
     
     T = 1/f0 # период, сек
-    ts = T/n # промежуток времени кантования, сек/отсчёт
+    n = time/ts # кол-во отсчетов
+    #ts = T/n # промежуток времени кантования, сек/отсчёт
     ShiftPhaseRadian = (ShiftPhaseDegree/180)*np.pi # сдвиг фазы в радианах(для рассчёта)
-    n_array = np.arange(0, n * n_T, 1) # отсчёты(массив)
+    n_array = np.arange(0, n, 1) # отсчёты(массив)
     
     print('A = ', A, ', амплитуда сигнала')
     print('ShiftPhaseDegree = ', ShiftPhaseDegree, ', градусы')
@@ -28,9 +29,10 @@ def getSinus(f0, A, n, n_T, ShiftZero, ShiftPhaseDegree):
     print('n = ', n, ', кол-во отсчётов')
     print('ts = ', ts, ',сек - период дискретизации')
     
-    _x = 2 * np.pi * f0 * n_array * ts
+    _tn = n_array * ts
+    _x = 2 * np.pi * f0 * _tn
     _y = A * np.sin(_x + ShiftPhaseRadian) + ShiftZero
 
     print(SelfFileName,'<- Sinus generate end.\n')
           
-    return _x, _y
+    return _tn, _y
